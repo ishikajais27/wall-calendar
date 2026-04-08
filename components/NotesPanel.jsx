fd26499f50aa37463dd631d01c211bcf40ededb7
ishikajais27 <ishikajais09876@gmail.com>
2026-04-09 03:20:57 +0530

    Final touch

--- components/NotesPanel.jsx
+++ components/NotesPanel.jsx
@@ -1,3 +1,3 @@
-// Notes & Goals are now integrated directly into Calendar.jsx
-// This file is kept for backwards compatibility
-export default function NotesPanel() { return null }
+export default function NotesPanel() {
+  return null
+}
